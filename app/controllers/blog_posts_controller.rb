95652d83b4818d1d58aae359e8d3b31fa9bd3e56
JohnTheodorou <giannistheodorou.uoi@gmail.com>
2024-09-23 23:17:26 +0300

    Create our basic blog

---
+++ app/controllers/blog_posts_controller.rb
@@ -0,0 +1,55 @@
+class BlogPostsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_block_post, except: [:index, :new, :create]
+
+    def index
+        @blog_posts = BlogPost.all
+    end
+
+    def show
+    end
+
+    def new
+        @blog_post = BlogPost.new
+    end
+
+    def create
+        @blog_post = BlogPost.new(blog_posts_params)
+        if @blog_post.save
+            redirect_to @blog_post
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        
+    end
+
+    def update
+        if @blog_post.update(blog_posts_params)
+            redirect_to @blog_post
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @blog_post.destroy
+        redirect_to root_path
+    end
+
+    private
+
+    def blog_posts_params
+        params.require(:blog_post).permit(:title, :text)
+    end
+
+    def set_block_post
+        @blog_post = BlogPost.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
+    end
+
+
+end
